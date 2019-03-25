2d945fbdc8542f1cd7173315dee7e4685ccc2955
Tectoc <tecotoc@gmail.com>
2019-03-24 21:50:25 -0600

    Service Worker y manifiesto configurados

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,19 @@
 module.exports = {
-    runtimeCompiler: true
+    runtimeCompiler: true,
+    pwa: {
+        workboxPluginMode: 'GenerateSW',
+        name: 'Odr',
+        appleMobileWebAppCapable: 'no',
+        workboxOptions: {
+            importWorkboxFrom: 'local',
+            maximumFileSizeToCacheInBytes: 50 * 1024 * 1024,
+            runtimeCaching:[{
+                urlPattern: '/',
+                handler: 'networkFirst',
+                options: {
+                    networkTimeoutSeconds: 30,
+                }
+            }]
+        }
+    }
 }
